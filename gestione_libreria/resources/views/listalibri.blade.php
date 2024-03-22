58a9a3ae47fe0f0f43a6fbdb0d71fbe3092df6f6
Francesco Zatti <francesco.zatti98gmail.com>
2024-03-22 13:04:53 +0100

    aggiornamento card libri (lista utente)

--- gestione_libreria/resources/views/listalibri.blade.php
+++ gestione_libreria/resources/views/listalibri.blade.php
@@ -7,36 +7,49 @@
 
     <div class="py-12">
         <div class="max-w-full mx-auto sm:px-6 lg:px-8"> 
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
                 @foreach($books as $book)
-                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex">
-                        <div class="w-full">
-                            <img src="{{ $book->image }}" class="w-full h-auto" alt="{{ $book->title }}">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex mb-3">
+                        <div class="col-md-2 col-sm-3">
+                            <img src="{{ $book->image }}" class="w-full h-auto p-3" alt="{{ $book->title }}">
                         </div>
-                        <div class="w-full p-6">
-                            <h3 class="text-xl font-semibold mb-2">{{ $book->title }}</h3>
-                            <p class="text-gray-700 mb-2">Author: {{ $book->author->name }}</p>
-                            <p class="text-gray-700 mb-2">Categories:
+                        <div class="p-6 col-md-7 col-sm-7">
+                            <p class="font-semibold mb-2 h2">{{ $book->title }}</p>
+                            <p class="text-gray-700 mb-2 ">{{ $book->author->name }}</p>
+                            <p class="text-secondary mb-2 fs-6">
                                 @foreach($book->categories as $category)
                                     {{ $category->name }}@if(!$loop->last),@endif
                                 @endforeach
                             </p>
-                            <p class="text-gray-700">{{ $book->plot }}</p>
+                            <div
+                            class="pt-2"
+                            style="border-top: 1px solid rgb(229, 231, 235);">
+                                <p class="text-gray-700">{{ $book->plot }}</p>
+                            </div>
                         </div>
-                        <div class="w-full flex-column items-center justify-between pt-4">
-                            <div class="text-end pe-4">
+                        <div class="flex-column justify-between pt-4 col-md-3 col-sm-2">
+                            
+                            <div class="pe-4 text-end d-flex align-items-center justify-content-end">
                                 @if ($book->status === "Available")
-                                    <span>tuo</span>
-                                    <i class="fas fa-circle text-success"></i>
+                                    <i class="fas fa-circle text-success me-1"></i>
+                                    <span class="text-success">SINCERAMENTE TUOOOOOOOOOO</span>
                                 @else
-                                    <span>suca</span>
-                                    <i class="fas fa-circle text-danger"></i>
+                                    <i class="fas fa-circle text-secondary me-1"></i>
+                                    <span class="text-secondary">QUANDO QUANDO QUANDO ANCHE SE A VOLTE</span>
                                 @endif
                             </div>
-
-                            <button class="btn btn-primary btn-sm mr-2 mb-2 w-full text-center hover:bg-blue-800 align-self-end">
+                            <div style="height: 70%;"></div>
+                            @if ($book->status === "Available")
+                                <div class="row">
+                                    <div class="col-8"></div>
+                                    <button class="btn btn-outline-danger btn mr-2 mb-2 align-self-end col-3">
+                                       MI NASCONDO 
+                                </button>
+                            @endif
+                            </div>
+                            <!-- <button class="btn btn-primary btn-sm mr-2 mb-2 hover:bg-blue-800 align-self-end">
                                 Button
-                            </button>
+                            </button> -->
                         </div>
                     </div>
                 @endforeach
