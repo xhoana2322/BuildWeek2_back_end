dd6781e98db0f52da33b6092e9d46ac75bc121c2
xhoana2322 <xhoana.beqiri23@gmail.com>
2024-03-22 12:28:23 +0100

    carousel

--- gestione_libreria/resources/views/components/cardHome.blade.php
+++ gestione_libreria/resources/views/components/cardHome.blade.php
@@ -1,11 +1,12 @@
-<div class="cardHome bg-light my-3 pt-1 pb-2 col-2 border rounded-2">
+
+<div class="cardHome bg-light my-3 pt-1 pb-2 col-2 border rounded-2 mx-3">
   <img src="..." class="mx-2 my-2 border" alt="..."
   style="height: 15rem; border: 2px solid red;">
   <div class="card-body mx-2">
     <div
     class="mb-1"
     style="border-bottom: 1px solid white">
-        <p class="card-title fw-bold h4">Titolo Libro</p>
+        <p class="card-title fw-bold h4">{{ $book->title }}</p>
         <p class="card-subtitle mb-2 text-muted h6">Autore</p>
     </div>
     <p class="card-text">Lorem ipsum dolor sit, amet consectetur<span>...</span></p>
--- gestione_libreria/resources/views/homepage.blade.php
+++ gestione_libreria/resources/views/homepage.blade.php
@@ -53,43 +53,51 @@
         <div class="container-fluid listaLibri">
             <section>
                 <div>
-                    <p class="h5 text-white mb-3 text-center text-sm-start">Categoria 1</p>
+                    @if($categories)
+                        @foreach($categories as $category)
+                            
+                    <p class="h5 mb-3 text-center text-sm-start">{{$category->CategoryName}}</p>
                 </div>
-            <div id="carouselCategory" class="carousel slide d-none d-sm-block">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <div class="d-flex">
-                            <x-cardHome />
-                            <x-cardHome />
-                            <x-cardHome />
+                <div id="carouselCategory" class="carousel slide d-none d-sm-block">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="d-flex">
+                                @foreach($books as $book)
+                                <div class="cardHome bg-light my-3 pt-1 pb-2 col-2 border rounded-2 mx-3">
+                                    <img src="..." class="mx-2 my-2 border" alt="..."
+                                    style="height: 15rem; border: 2px solid red;">
+                                    <div class="card-body mx-2">
+                                        <div
+                                        class="mb-1"
+                                        style="border-bottom: 1px solid white">
+                                            <p class="card-title fw-bold h4">{{ $book->title }}</p>
+                                            <p class="card-subtitle mb-2 text-muted h6">Autore</p>
+                                        </div>
+                                        <p class="card-text">Lorem ipsum dolor sit, amet consectetur<span>...</span></p>
+                                        <a href="#">Scopri di più</a>
+                                        <div class="d-flex justify-content-between align-items-center mt-2 mx-2">
+                                            <a href="#" class="btn btn-primary">Prenota</a>
+                                            <p class="text-success fw-bold">Disponibile</p>
+                                        </div>
+                                    </div>
+                                    </div>
+                                @endforeach
+                            </div>
                         </div>
                     </div>
-                    <div class="carousel-item">
-                        <div class="d-flex">
-                            <x-cardHome />
-                            <x-cardHome />
-                            <x-cardHome />
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="d-flex">
-                            <x-cardHome />
-                            <x-cardHome />
-                            <x-cardHome />
-                        </div>
-                    </div>
+                    <button class="carousel-control-prev text-dark" type="button" data-bs-target="#carouselCategory"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon me-5" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next text-dark" type="button" data-bs-target="#carouselCategory"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon ms-5" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
-                <button class="carousel-control-prev text-dark" type="button" data-bs-target="#carouselCategory"
-                    data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon me-5" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next text-dark" type="button" data-bs-target="#carouselCategory"
-                    data-bs-slide="next">
-                    <span class="carousel-control-next-icon ms-5" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
+                @endforeach
+                @endif
             </section>
         </div>
     </x-app-layout>
