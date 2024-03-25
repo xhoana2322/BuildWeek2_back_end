e5c09b9ab6a49993b800a2f5df00b2fc268d4cab
SnIvanova <1snizhanaivanova@gmail.com>
2024-03-25 15:38:14 +0100

    Update show.blade.php

--- gestione_libreria/resources/views/books/show.blade.php
+++ gestione_libreria/resources/views/books/show.blade.php
@@ -1,3 +1,85 @@
+<style>
+    @property --k {
+    syntax: '<number>';
+    initial-value: -1;
+    inherits: true;
+}
+
+.spec {
+    --m: 1;
+    grid-area: 2/ 1/ span 1/ span 2;
+    position: relative;
+    margin: 1em;
+    border: solid 2em transparent;
+    padding: 1.5em 3.25em;
+    border-radius: 5em;
+    box-shadow: inset 0 0 1px #23429e;
+    background:
+        radial-gradient(#92372d, 67.5%, #F87060) padding-box,
+        radial-gradient(#a0c2ed, 35%, #a0c2ed00 70%) 50% 0/ 80% 50% repeat-y;
+    color: #f2fdfe;
+    font: 700 1.5em/ 1 montserrat, sans-serif;
+
+    #slow:checked ~ & {
+        --m: 5;
+    }
+}
+
+@keyframes k {
+    0%, 33.3% {
+        --k: 1;
+    }
+}
+
+.particle {
+    --f: 1;
+    --pos-k: max(0, var(--k));
+    --neg-k: max(0, -1 * var(--k));
+    --low-c: min(1, 4 * (1 - var(--pos-k)));
+    --abs-d: max(var(--neg-k) - .5, .5 - var(--neg-k));
+    --mov-f: var(--pos-k);
+    display: grid;
+    position: absolute;
+    left: var(--x);
+    top: var(--y);
+    rotate: var(--a);
+    animation: k calc(var(--m) * 1.5s) linear calc(var(--m) * var(--t, 0) * 1.5s) infinite;
+
+    @supports (scale: sqrt(4)) {
+        --mov-f: sqrt(var(--pos-k));
+    }
+
+    &::before,
+    &::after {
+        grid-area: 1/ 1;
+        width: .75em;
+        aspect-ratio: 1;
+    }
+
+    &::before {
+        --sa: calc(min(1, 1 - 2 * min(.5, var(--mov-f))) * 45deg);
+        border-radius: calc(1.25 * min(.8, var(--mov-f)) * 50%) 50% 50%;
+        transform-origin: 0 0;
+        translate: calc(var(--mov-f) * var(--d));
+        rotate: -45deg;
+        scale: var(--f);
+        transform: skew(var(--sa), var(--sa));
+        opacity: var(--low-c);
+        filter: Saturate(var(--low-c));
+        background: radial-gradient(at 85% 85%, #bad9fa, #3e66a4 75%);
+        content: '';
+    }
+
+    &::after {
+        translate: -50% -50%;
+        scale: calc(var(--f) * (1 - 2 * var(--abs-d)));
+        text-align: center;
+        filter: blur(.5px);
+        content: '✦';
+    }
+}
+</style>
+
 <x-app-layout>
     <div class="d-flex h-55vh w-100 items-center justify-content-between px-10 my-4">
         <div class="max-w-4xl mx-auto px-6 px-lg-8">
@@ -6,6 +88,7 @@
                     <!-- Book Image -->
                     <div class="col-12 col-md-6 col-lg-4 m-2 d-flex justify-content-center bg-fixed">
                         <img src="{{ $book->image }}" alt="{{ $book->title }}" loading="lazy" class="inset-0 ms-auto w-75 h-100 rounded-bottom-start object-cover object-center mx-2">
+                        <span class="absolute ">{{ $book->category->CategoryName ?? 'Category not available' }}</span>
                     </div>
                     <!-- Book Details -->
                     <div class="col-12 col-md-6 col-lg-6 container my-4 mx-auto mt-28">
@@ -25,33 +108,7 @@
                                             c-1.532-1.494-0.687-4.096,1.431-4.403l12.091-1.757c0.841-0.122,1.568-0.65,1.944-1.412l5.407-10.956
                                             C22.602,0.567,25.338,0.567,26.285,2.486z"/>
                                     </svg>
-                                    <svg  version="1.1" id="Capa_1"   
-                                        viewBox="0 0 47.94 47.94" width="20" height="20">
-                                        <path style="fill:#ED8A19;" d="M26.285,2.486l5.407,10.956c0.376,0.762,1.103,1.29,1.944,1.412l12.091,1.757
-                                            c2.118,0.308,2.963,2.91,1.431,4.403l-8.749,8.528c-0.608,0.593-0.886,1.448-0.742,2.285l2.065,12.042
-                                            c0.362,2.109-1.852,3.717-3.746,2.722l-10.814-5.685c-0.752-0.395-1.651-0.395-2.403,0l-10.814,5.685
-                                            c-1.894,0.996-4.108-0.613-3.746-2.722l2.065-12.042c0.144-0.837-0.134-1.692-0.742-2.285l-8.749-8.528
-                                            c-1.532-1.494-0.687-4.096,1.431-4.403l12.091-1.757c0.841-0.122,1.568-0.65,1.944-1.412l5.407-10.956
-                                            C22.602,0.567,25.338,0.567,26.285,2.486z"/>
-                                    </svg>
-                                    <svg  version="1.1" id="Capa_1"   
-                                        viewBox="0 0 47.94 47.94" width="20" height="20">
-                                        <path style="fill:#ED8A19;" d="M26.285,2.486l5.407,10.956c0.376,0.762,1.103,1.29,1.944,1.412l12.091,1.757
-                                            c2.118,0.308,2.963,2.91,1.431,4.403l-8.749,8.528c-0.608,0.593-0.886,1.448-0.742,2.285l2.065,12.042
-                                            c0.362,2.109-1.852,3.717-3.746,2.722l-10.814-5.685c-0.752-0.395-1.651-0.395-2.403,0l-10.814,5.685
-                                            c-1.894,0.996-4.108-0.613-3.746-2.722l2.065-12.042c0.144-0.837-0.134-1.692-0.742-2.285l-8.749-8.528
-                                            c-1.532-1.494-0.687-4.096,1.431-4.403l12.091-1.757c0.841-0.122,1.568-0.65,1.944-1.412l5.407-10.956
-                                            C22.602,0.567,25.338,0.567,26.285,2.486z"/>
-                                    </svg>
-                                    <svg  version="1.1" id="Capa_1"   
-                                        viewBox="0 0 47.94 47.94" width="20" height="20">
-                                        <path style="fill:#ED8A19;" d="M26.285,2.486l5.407,10.956c0.376,0.762,1.103,1.29,1.944,1.412l12.091,1.757
-                                            c2.118,0.308,2.963,2.91,1.431,4.403l-8.749,8.528c-0.608,0.593-0.886,1.448-0.742,2.285l2.065,12.042
-                                            c0.362,2.109-1.852,3.717-3.746,2.722l-10.814-5.685c-0.752-0.395-1.651-0.395-2.403,0l-10.814,5.685
-                                            c-1.894,0.996-4.108-0.613-3.746-2.722l2.065-12.042c0.144-0.837-0.134-1.692-0.742-2.285l-8.749-8.528
-                                            c-1.532-1.494-0.687-4.096,1.431-4.403l12.091-1.757c0.841-0.122,1.568-0.65,1.944-1.412l5.407-10.956
-                                            C22.602,0.567,25.338,0.567,26.285,2.486z"/>
-                                    </svg>
+                                  
                                    
                                 </div>
                                 <div class="pt-3">
@@ -74,9 +131,23 @@
                             </div>
                         </div>
                         <div class="d-flex justify-content-center">
-                            <a href="#" class="btn btn-warning tasto_dettaglio w-50 p-2 mt-3">
-                            Reserve Now
-                            </a>   
+                            <a href="#" class="btn btn-warning tasto_dettaglio w-50 p-2 mt-3 spec">
+                                Reserve Now
+                                <span class="wrap" aria-hidden="true">
+                                    <span class="particle" style="--a: -45deg; --x: 53%; --y: 15%; --d: 4em; --f: .7; --t: .15"></span>
+                                    <span class="particle" style="--a: 150deg; --x: 40%; --y: 70%; --d: 7.5em; --f: .8; --t: .08"></span>
+                                    <span class="particle" style="--a: 10deg; --x: 90%; --y: 65%; --d: 7em; --f: .6; --t: .25"></span>
+                                    <span class="particle" style="--a: -120deg; --x: 15%; --y: 10%; --d: 4em"></span>
+                                    <span class="particle" style="--a: -175deg; --x: 10%; --y: 25%; --d: 5.25em; --f: .6; --t: .32"></span>
+                                    <span class="particle" style="--a: -18deg; --x: 80%; --y: 25%; --d: 4.75em; --f: .5; --t: .4"></span>
+                                    <span class="particle" style="--a: -160deg; --x: 30%; --y: 5%; --d: 9em; --f: .9; --t: .5"></span>
+                                    <span class="particle" style="--a: 175deg; --x: 9%; --y: 30%; --d: 6em; --f: .95; --t: .6"></span>
+                                    <span class="particle" style="--a: -10deg; --x: 89%; --y: 25%; --d: 4.5em; --f: .55; --t: .67"></span>
+                                    <span class="particle" style="--a: -140deg; --x: 40%; --y: 10%; --d: 5em; --f: .85; --t: .75"></span>
+                                    <span class="particle" style="--a: 90deg; --x: 45%; --y: 65%; --d: 4em; --f: .5; --t: .83"></span>
+                                    <span class="particle" style="--a: 30deg; --x: 70%; --y: 80%; --d: 6.5em; --f: .75; --t: .92"></span>
+                                </span>
+                            </a>
                         </div>
                     </div>
                 </div>
